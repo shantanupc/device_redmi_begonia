af7488fbac2dd85a4c74e3ed7c1957c5d769195c
Tim Zimmermann <tim@linux4.de>
2022-06-13 09:19:40 +0530

    Begonia: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>
    Signed-off-by: SHADOW <33841315+SHADOW-REX@users.noreply.github.com>
    Signed-off-by: ShantanuPC <shantanuchimote42@gmail.com>

--- device.mk
+++ device.mk
@@ -147,6 +147,9 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
