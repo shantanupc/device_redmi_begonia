0e041c47a08dd8730715f25dae560dc1254a1fd6
ShantanuPC <shantanuchimote42@gmail.com>
2023-11-29 00:55:30 +0530

    begonia: Initialize AlphaDroid

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -15,11 +15,33 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 
-# Inherit some common VoltageOS stuff
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+# Inherit some common Alpha stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_DOESNT_LIKE_FLIPENDO := true
 TARGET_ENABLE_BLUR := true
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GAPPS := true
+TARGET_INCLUDE_GOOGLE_TELEPHONY := false
+TARGET_INCLUDE_PIXEL_FRAMEWORK := false
+TARGET_INCLUDE_GOOGLE_CAMERA := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_GMAIL := false
+TARGET_INCLUDE_GOOGLE_MAPS := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+TARGET_DEFAULT_ADB_ENABLED := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := ShantanuPC
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
@@ -38,12 +60,9 @@ TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Official-ify
-VOLTAGE_BUILD_TYPE := OFFICIAL
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := voltage_begonia
+PRODUCT_NAME := lineage_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
