3da4d4a97ca9a55fabf65a4c6867233cc3752e4c
Aaron Kling <webgeek1234@gmail.com>
2024-10-14 20:40:10 +0530

    begonia: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: ShantanuPC <shantanuchimote42@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -23,6 +23,14 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+	cat <<EOF >>"$1"
+		"device/redmi/begonia",
+		"hardware/mediatek",
+		"hardware/mediatek/libmtkperf_client"
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
