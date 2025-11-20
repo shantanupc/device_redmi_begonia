f3d1801529ca59e78932676483dcf7fd2de02bbe
claxten10 <claxten10@gmail.com>
2025-11-20 22:54:35 +0000

    begonia: Switch to dummy libaedv
    Change-Id: I0f87e8536103cd59b41a2d8ee322c861a352e31d
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: ShantanuPC <shantanuchimote42@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/libaedv \
     hardware/lineage/interfaces/power-libperfmgr \
     $(LOCAL_PATH)
 
